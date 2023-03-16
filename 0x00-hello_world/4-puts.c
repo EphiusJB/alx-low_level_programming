11b64be0fb66560445662973e92ad85507c2a220
EphiusJB <cloudjoseph797@gmail.com>
2023-03-16 22:45:02 +0200

    using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+intmain(void)
+{
+puts("\"Programmingis like building a multilingual puzzle");
+
+return (0);
+
+}
