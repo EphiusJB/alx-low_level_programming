71f2610d5b5f85c5d542c2eef0677183092e1341
EphiusJB <cloudjoseph797@gmail.com>
2023-03-17 18:04:15 +0200

    base 16 in lowercase print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int ch;
+for (ch = 48; ch < 58; ch++)
+{
+putchar(ch);
+}
+for (ch = 97; ch < 103; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+
+return (0);
+}
+
