9bbbef7ed18a2d6a66f7a6ebff44bfb9190b549e
EphiusJB <cloudjoseph797@gmail.com>
2023-03-17 12:12:37 +0200

    printing the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int ch = 97;
+for (ch = 97; ch < 123; ch++)
+putchar(ch);
+
+return (0);
+}
+
