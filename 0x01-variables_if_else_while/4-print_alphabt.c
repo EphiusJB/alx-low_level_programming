5b955ca141a1dbc7875cdbfdd09b159ea31dbcd9
EphiusJB <cloudjoseph797@gmail.com>
2023-03-17 15:02:53 +0200

    without q or e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int ch;
+char chx = '\n';
+for (ch = 97; ch < 123; ch++)
+{
+if (ch == 101 || ch == 113)
+putchar("");
+else
+putchar(ch);
+}
+putchar(chx);
+
+return (0);
+}
+
