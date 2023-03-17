7531f4cb50815f1b4c97c16497eddc42ca3af905
EphiusJB <cloudjoseph797@gmail.com>
2023-03-17 18:11:13 +0200

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int ch;
+for (ch = 48; ch < 58; ch++)
+{
+putchar(ch);
+if (ch == 57)
+{
+putchar('$');
+}
+else
+{
+putchar(',');
+putchar(32);
+}
+}
+
+return (0);
+}
+
