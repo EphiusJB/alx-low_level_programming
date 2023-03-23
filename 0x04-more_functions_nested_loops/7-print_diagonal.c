362483020e49f944e034ba8a053de0709d46f965
EphiusJB <cloudjoseph797@gmail.com>
2023-03-23 14:38:09 +0200

    diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,14 +16,14 @@ if (n > 0)
 while (m < n)
 {
 for (i = 0; i < m; i++)
-_putchar(' ');
-_putchar('\');
-_putchar('\n');
+_putchar(32);
+_putchar(92);
+_putchar(10);
 m++;
 }
 }
 else
 {
-_putchar('\n');
+_putchar(10);
 }
 }
