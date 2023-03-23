b72b7eb6d2560d87ccee510cfef650cad520931e
EphiusJB <cloudjoseph797@gmail.com>
2023-03-23 14:04:40 +0200

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int ch;
+char chx = '\n';
+for (ch = 48; ch < 58; ch++)
+{
+putchar(ch);
+}
+putchar(chx);
+
+return (0);
+}
+
