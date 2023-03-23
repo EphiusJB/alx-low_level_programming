0dd6f5f58448b57973886feb56b59245dd00a65c
EphiusJB <cloudjoseph797@gmail.com>
2023-03-23 14:24:06 +0200

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - function
+ *
+ * @n: parameter
+ *
+ * Return: return value
+ */
+void print_line(int n)
+{
+if (n > 0)
+{
+while (n > 0)
+_putchar('_');
+}
+_putchar('\n');
+}
