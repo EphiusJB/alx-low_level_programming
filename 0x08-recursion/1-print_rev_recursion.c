bf72559e999475708469f755b3bedbf4a3412290
EphiusJB <cloudjoseph797@gmail.com>
2023-04-05 16:43:49 +0200

    reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+** _print_rev_recursion - Prints a string in reverse.
+*
+** @s:Pointer to String
+*
+** Return: Void
+*
+**/
+
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_puts_recursion(s + 1);
+putchar(*s);
+}
+else
+{
+putchar('\n');
+}
+}
