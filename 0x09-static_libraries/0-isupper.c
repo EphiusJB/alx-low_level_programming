868708ce314b616c93d2262d75db31cbc03417ef
EphiusJB <cloudjoseph797@gmail.com>
2023-04-07 21:39:33 +0200

    libmy static library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercaseor lowercase
+ * @c: int type number
+ * Return: 1 if uppercase, else 0
+ */
+
+int _isupper(int c)
+{
+if (c > 64 && c < 91)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
