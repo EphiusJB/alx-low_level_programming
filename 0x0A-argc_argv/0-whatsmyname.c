ed7d0aa93ebb9c3025c413f0883fba492fdc8801
EphiusJB <cloudjoseph797@gmail.com>
2023-04-08 23:00:57 +0200

    what's my name program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+* main - Entry point
+*
+* @argc: commandline argument count
+*
+* @argv: array of arguments
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+printf("programname:%s", argv[0]);
+
+return (0);
+}
+
