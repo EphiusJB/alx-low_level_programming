794e3e938f5f4432dc95a402ce478129d7534fb7
EphiusJB <cloudjoseph797@gmail.com>
2023-04-09 00:01:08 +0200

    what's my name program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
 
 /**
@@ -8,13 +9,24 @@
 *
 * @argv: array of arguments
 *
-* Return: 0
+* @strLength: length of the program name returned by ragv[0]
+*
+* @count: loop counter
+*
+* Return: returns the valu of argc
 */
 
 int main(int argc, char *argv[])
 {
-printf("programname:%s", argv[0]);
-
-return (0);
+int strLength, count;
+count = 0;
+strLength = strlen(argv[0]);
+while (count < strLength)
+{
+_putchar(argv[0][count]);
+count++;
 }
+return (argc);
+}
+
 
