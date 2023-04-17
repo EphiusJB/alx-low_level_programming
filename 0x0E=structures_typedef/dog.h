0a39841f1262b91924cda043dcf07f0bf76354d4
EphiusJB <cloudjoseph797@gmail.com>
2023-04-17 16:22:00 +0200

    a struct of dog

---
+++ 0x0E=structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H_INCLUDED
+#define DOG_H_INCLUDED
+
+/**
+ * struct dog - A type of struct dog
+ *
+ * @name: char string to hold name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
