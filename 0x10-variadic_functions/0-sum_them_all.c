4ddfab0c702ef9bac919d03d54083958706a5545
EphiusJB <cloudjoseph797@gmail.com>
2023-04-21 11:57:46 +0200

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,10 +11,10 @@ int sum_them_all(const unsigned int n, ...)
 unsigned int i;
 int sum = 0;
 va_list list;
-   if (n == 0)
-   {
-   return (0);    
-   }
+if (n == 0)
+{
+return (0);
+}
 
 va_start(list, n);
 
